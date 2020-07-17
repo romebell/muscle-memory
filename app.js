fe665acec3ec7a8f2cb165ece1d13f7b4db6c75b
Rome Bell <romebell5@gmail.com>
2020-07-17 17:07:49 -0400

    [variables] Add 5 variables

---
+++ app.js
@@ -0,0 +1,27 @@
+// 5 variables of different data types
+// string
+// number
+// array
+// boolean
+// object (4 different key/value pairs) [string], [number], [array], [boolean]
+
+let sentence = 'Today, it is Friday!';
+console.log(sentence);
+
+let age = 32;
+console.log(age);
+
+const motorcycles = ['Kawasaki', 'Yamaha', 'Suzuski'];
+console.log(motorcycles);
+
+let doesThisWork = true;
+console.log(doesThisWork);
+
+const lakers = {
+    headquarters: 'Los Angeles',
+    numberOfPlayers: 11,
+    players: ['LBJ', 'Anthony Davis', 'Danny Green'],
+    areChampions: true
+}
+
+console.log(lakers);
