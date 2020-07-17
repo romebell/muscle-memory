9d397a44edcf78a698bcbabf74b438f8d9d72725
Rome Bell <romebell5@gmail.com>
2020-07-17 17:53:42 -0400

    [functions] Add 5 functions

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
 let sentence = 'Today, it is Friday!';
 console.log(sentence);
 
+let name = 'Rome';
+
 let age = 32;
 console.log(age);
 
@@ -25,3 +27,68 @@ const lakers = {
 }
 
 console.log(lakers);
+
+// make 10 different functions
+// one of your function need to pass in an array
+// one of your functions need to pass in a callback
+// .... string
+// .... object
+// ... boolean
+// ... freestyle
+
+// TEST YOUR CODE!!!
+
+function displayCoolBikes(array) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let bike = array[i];
+
+        if (bike.length > 7) {
+            result.push(bike);
+        }
+    }
+
+    return result;
+}
+
+// console.log(displayCoolBikes(motorcycles));
+
+function removeFromArray(callback, array) {
+    let result = callback(array)[0];
+    console.log(result);
+}
+
+removeFromArray(displayCoolBikes, motorcycles);
+
+function printSentence(string) {
+    console.log(string);
+}
+
+printSentence(sentence);
+printSentence(name);
+
+function printPlayers(object) {
+    console.log(object.players);
+}
+
+printPlayers(lakers);
+
+function printEachPlayer(object) {
+    object.players.forEach(eachPlayer => {
+        console.log(eachPlayer);
+    });
+}
+
+printEachPlayer(lakers);
+
+function doesThisReallyWork(boolean) {
+    if (boolean) {
+        console.log('this works');
+    } else {
+        console.log('This does not work');
+    }
+}
+
+doesThisReallyWork(doesThisWork);
+
